1a7a0627788dbe5bbde74429593e6630ab3305ff
gesteban <gesteban@unizar.es>
2014-11-20 17:53:46 +0100

    Añadida clase de generado de RDF, clases para vocabularios.

--- Licitaciones/src/es/unizar/contsem/codice/parser/Test.java
+++ Licitaciones/src/es/unizar/contsem/codice/parser/Test.java
@@ -2,44 +2,28 @@ package es.unizar.contsem.codice.parser;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 
 import org.dom4j.Document;
-import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 import es.unizar.contsem.codice.parser.database.DatabaseManager;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
+
 		DatabaseManager db = new DatabaseManager();
 		String firstXML = db.getFirstXML();
-		System.out.println(firstXML);
 
 		SAXReader reader = new SAXReader();
 		Document document = reader.read(new ByteArrayInputStream(firstXML
 				.getBytes(StandardCharsets.UTF_8)));
 
-		Element root = document.getRootElement();
-
-		// iterate through child elements of root
-		for (Iterator i = root.elementIterator(); i.hasNext();) {
-			Element element = (Element) i.next();
-			System.out.println(element.toString());
-		}
-
-		// // iterate through child elements of root with element name "foo"
-		// for ( Iterator i = root.elementIterator( "foo" ); i.hasNext(); ) {
-		// Element foo = (Element) i.next();
-		// // do something
-		// }
-		//
-		// // iterate through attributes of root
-		// for ( Iterator i = root.attributeIterator(); i.hasNext(); ) {
-		// Attribute attribute = (Attribute) i.next();
-		// // do something
-		// }
+		Model model = ModelFactory.createDefaultModel();
+		CodiceToPprocParser.parseCodiceXML(model, document);
 
 	}
 
