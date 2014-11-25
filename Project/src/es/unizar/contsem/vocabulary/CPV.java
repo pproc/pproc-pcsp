ad390ee45c15c0634ded6eede83f6b6eea533fc6
gesteban <gesteban@unizar.es>
2014-11-25 17:21:58 +0100

    Clase vocabulario CPV.

---
+++ Project/src/es/unizar/contsem/vocabulary/CPV.java
@@ -0,0 +1,23 @@
+package es.unizar.contsem.vocabulary;
+
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+
+public class CPV {
+
+	protected static final String uri = "http://purl.org/cpv/2008/";
+
+	/**
+	 * returns the URI for this schema
+	 * 
+	 * @return the URI for this schema
+	 */
+	public static String getURI() {
+		return uri;
+	}
+
+	public static final Resource code(String code) {
+		return ResourceFactory.createResource(uri + "code-" + code);
+	}
+
+}
