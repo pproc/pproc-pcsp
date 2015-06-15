774e32c6af4934b7ed0a31d056582d5c91eebbfb
Guillermo Esteban <ggestebanp@gmail.com>
2015-06-15 10:00:20 +0200

    Removed some lines from vocabularies

--- Project/src/es/unizar/pproc/vocabulary/S.java
+++ Project/src/es/unizar/pproc/vocabulary/S.java
@@ -1,6 +1,5 @@
 package es.unizar.pproc.vocabulary;
 
-import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
@@ -41,14 +40,5 @@ public class S {
 	public static final Property email = property("email");
 	public static final Property location = property("location");
 	public static final Property startDate = property("startDate");
-
-	/**
-	 * The same items of vocabulary, but at the Node level, parked inside a nested class so that there's a simple way to
-	 * refer to them.
-	 */
-	public static final class Nodes {
-		public static final Node Organization = ORG.Organization.asNode();
-		public static final Node hasSite = ORG.hasSite.asNode();
-	}
 
 }
